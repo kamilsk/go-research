e4860b37170acb4f39734a02319f4990a0cd8ad7
Kamil Samigullin <kamil@samigullin.info>
2018-09-10 20:36:42 +0300

    update deps, import lost packages

--- logging/dep.go
+++ logging/dep.go
@@ -5,6 +5,13 @@ import (
 	_ "github.com/francoispqt/onelog"
 	_ "github.com/inconshreveable/log15"
 	_ "github.com/rs/zerolog"
+	_ "github.com/rs/zerolog/diode"
+	_ "github.com/rs/zerolog/hlog"
+	_ "github.com/rs/zerolog/journald"
+	_ "github.com/rs/zerolog/log"
 	_ "github.com/sirupsen/logrus"
+	_ "github.com/sirupsen/logrus/hooks/syslog"
+	_ "github.com/sirupsen/logrus/hooks/test"
 	_ "go.uber.org/zap"
+	_ "go.uber.org/zap/zapgrpc"
 )
